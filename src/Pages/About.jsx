713c35331b2caa8a1210f1849c473fff0592c2ce
Mansi agarwal <mansiagarwal293@gmail.com>
2025-02-09 16:31:05 +0530

    about added

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,16 +1,46 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Button, Text } from "@chakra-ui/react";
 import { Image } from "@chakra-ui/react";
+import "../style/about.css";
 const About = () => {
   return (
-    <div className="about_container">
-      <Text>About</Text>
-      <div className="left_container">
-        <Image src="" />
-      </div>
-      <div className="right_container">
-        <Box>
-          <Text></Text>
-        </Box>
+    <div>
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Text fontSize="3xl" fontWeight="bold" p={5}>
+          About
+        </Text>
+      </Box>
+
+      <div className="about_container">
+        <div className="left_container">
+          <Image
+            src="https://raw.githubusercontent.com/mansiagar/Mansi-Portfolio-/673c4fdcd211b66950d6653962981ca0c0c93721/src/assets/profile.jpg"
+            alt="Profile Picture"
+            boxSize="300px"
+            borderRadius="full"
+          />
+        </div>
+        <div className="right_container">
+          <Box>
+            <Text>
+              A passionate Front-End Developer with expertise in React.js,
+              JavaScript, and modern UI libraries like Chakra UI and Material
+              UI, dedicated to creating responsive, interactive, and visually
+              appealing web applications. With a strong foundation in HTML, CSS,
+              and JavaScript, the focus is on writing clean, efficient, and
+              maintainable code while ensuring a seamless user experience.
+              Always eager to learn new technologies, optimize performance, and
+              bring creative ideas to life, with the goal of contributing to
+              innovative projects and collaborating with teams to develop
+              high-quality web solutions that make a difference. 🚀
+            </Text>
+            <Button>Resume</Button>
+          </Box>
+        </div>
       </div>
     </div>
   );
