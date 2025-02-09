673c4fdcd211b66950d6653962981ca0c0c93721
Mansi agarwal <mansiagarwal293@gmail.com>
2025-02-09 16:05:42 +0530

    home completed

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,5 +1,19 @@
+import { Box, Text } from "@chakra-ui/react";
+import { Image } from "@chakra-ui/react";
 const About = () => {
-  return <div>This is About Page</div>;
+  return (
+    <div className="about_container">
+      <Text>About</Text>
+      <div className="left_container">
+        <Image src="" />
+      </div>
+      <div className="right_container">
+        <Box>
+          <Text></Text>
+        </Box>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,32 +1,45 @@
-import "../style/animation.css"; // Import Animista animations
+import "../style/animation.css";
 import { Box, Text } from "@chakra-ui/react";
-import Navbar from "../Component/Navbar"; // Make sure your Navbar component is correctly imported
+
+import Navbar from "../Component/Navbar";
+import About from "./About";
 
 const Home = () => {
   return (
-    <Box
-      className="color-change-2x" // Animista animation
-      maxW="100%" // Set max width
-      h="600px"
-      // mx="auto" // Center the box horizontally
-      p={8} // Add padding inside the container
-      boxShadow="lg" // Add shadow for depth
-      borderRadius="lg" // Rounded corners
-      bg="gray.100" // Light background
-    >
-      <Navbar />
+    <div>
       <Box
-        display="flex" // Use flexbox for alignment
-        flexDirection="column" // Stack items vertically
-        justifyContent="center" // Center content vertically
-        alignItems="center" // Center content horizontally
-        mt={100}
+        className="color-change-2x" // Animista animation
+        maxW="100%" // Set max width
+        h="600px"
+        p={8} // Add padding inside the container
+        boxShadow="lg" // Add shadow for depth
+        borderRadius="lg" // Rounded corners
+        bg="gray.100" // Light background
       >
-        <Text className="focus-in" fontSize="3xl" fontWeight="bold">
-          Welcome to my portfolio✨
-        </Text>
+        <Navbar />
+        <Box
+          display="flex" // Use flexbox for alignment
+          flexDirection="column" // Stack items vertically
+          justifyContent="center" // Center content vertically
+          alignItems="center" // Center content horizontally
+          mt={100}
+        >
+          <Text
+            className="focus-in"
+            fontSize="3xl"
+            fontWeight="bold"
+            p={5}
+            mt={10}
+          >
+            ✨Hi, I&apos;m Mansi Agrawal✨
+          </Text>
+          <Text className="focus-in" fontSize="3xl" fontWeight="bold" p={5}>
+            A Passionate Front-End Developer 🚀
+          </Text>
+        </Box>
       </Box>
-    </Box>
+      <About />
+    </div>
   );
 };
 
