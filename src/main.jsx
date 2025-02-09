89a592251248c1420cf2f56679ff82b7bb257a55
Mansi agarwal <mansiagarwal293@gmail.com>
2025-02-09 20:29:19 +0530

    gh-pages added

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import { Provider } from "@/components/ui/provider";
 
 import { createRoot } from "react-dom/client";
-
-import App from "./App.jsx";
-import { HashRouter } from "react-router-dom";
+import Portfolio from "./Demo.jsx";
+//import App from "./App.jsx";
+//import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <HashRouter>
-    <Provider>
-      <App />
-    </Provider>
-  </HashRouter>
+  // <HashRouter>
+  //   <Provider>
+  //     <App />
+  //   </Provider>
+  // </HashRouter>
+  <Provider>
+    <Portfolio />
+  </Provider>
 );
