5b26ae6cfaee68b1a322bf5864f9f2aa494870cf
Mansi agarwal <mansiagarwal293@gmail.com>
2025-02-14 10:30:38 +0530

    project section completed

--- src/Pages/Project.jsx
+++ src/Pages/Project.jsx
@@ -10,38 +10,36 @@ const Project = () => {
         justifyContent="center"
         alignItems="center"
       >
-        <Text fontSize="3xl" fontWeight="bold" p={5}>
+        <Text fontSize="3xl" fontWeight="bold">
           My PROJECTS
         </Text>
       </Box>
 
       <div className="project_container">
         <div className="project_1">
-          <div className="image_container">
+          <div className="image_container_1">
             <Image
               className="image"
-              src="https://raw.githubusercontent.com/mansiagar/Mansi-Portfolio-/003e3552d80711c4e8c37466978f98871dcdcd62/src/assets/Project%201.jpg
-"
+              src="https://raw.githubusercontent.com/mansiagar/Mansi-Portfolio-/a33c59c4c8cb2f47fd45cfdd3beba3e2d04fb945/src/assets/Project%201.jpg"
             />
           </div>
-          <div className="description_container">
-            <Text display={"flex"} justifyContent={"center"}>
-              The Resume Builder App is an open-source, user-friendly web
-              application designed to help users create professional resumes
-              effortlessly. Developed using React.js, Ant Design, and Bootstrap,
-              this project offers multiple pre-designed templates where users
-              can input their details and generate a polished resume in PDF
-              format. With a clean and intuitive UI, the application provides a
-              real-time preview of the resume, ensuring a seamless experience.
-              The PDF export functionality allows users to download their
-              resumes instantly. This project was built during my internship at
-              Xenosis IT Solution and is open-source, enabling community
-              contributions for further enhancements. The application is fully
-              responsive, making it accessible across all devices. By
-              integrating modern UI libraries and optimizing for performance,
-              this project showcases my expertise in React.js, front-end
-              development, and UI/UX design. Check out the project on GitHub and
-              explore the live demo to see it in action.
+          <div className="description_container_1">
+            <Text
+              className="dec_text"
+              display={"flex"}
+              justifyContent={"center"}
+            >
+              The Resume Builder App is an open-source web application that
+              simplifies resume creation with pre-designed templates and
+              real-time previews. Built with React.js, Ant Design, and
+              Bootstrap, it allows users to input details and download a PDF
+              resume instantly. Developed during my internship at Xenosis IT
+              Solution, the app is fully responsive and open to community
+              contributions. This project highlights my skills in React.js,
+              front-end development, and UI/UX design.
+            </Text>
+            <Text className="highlight">
+              Explore it on GitHub and check out the live demo!{" "}
             </Text>
             <div className="button_container">
               <Button>
@@ -62,7 +60,47 @@ const Project = () => {
           </div>
         </div>
         <div className="project_2">
-          <div className="image_container">
+          {/* dec_start */}
+          <div className="description_container">
+            <Text
+              display={"flex"}
+              justifyContent={"center"}
+              className="dec_text"
+              marginLeft={10}
+            >
+              The Trend Bazzar is a women-focused e-commerce platform designed
+              for a seamless shopping experience. Built using HTML, CSS, and
+              JavaScript, with Glitch API handling the backend, this project
+              offers a responsive design, ensuring accessibility across all
+              devices. Users can browse a curated collection of women&apos;s
+              fashion, add items to their cart, and explore stylish trends
+              effortlessly. The platform emphasizes smooth navigation,
+              interactive UI, and optimized performance. This project highlights
+              my skills in front-end development, API integration, and
+              responsive web design.
+            </Text>
+            <Text className="highlight" marginLeft={10}>
+              Explore it on GitHub and check out the live demo! 💃🛍️
+            </Text>
+            <div className="button_container_2">
+              <Button>
+                <a
+                  href="https://mansiagar.github.io/resume_builder_app/"
+                  target="blank"
+                ></a>
+                Live Demo
+              </Button>
+              <Button>
+                <a
+                  href="https://github.com/mansiagar/resume_builder_app.git"
+                  target="blank"
+                ></a>
+                GitHub Link
+              </Button>
+            </div>
+          </div>
+          {/* dec_end */}
+          <div className="image_container_2">
             <Image
               className="image"
               src="https://raw.githubusercontent.com/mansiagar/Mansi-Portfolio-/003e3552d80711c4e8c37466978f98871dcdcd62/src/assets/Project%202.jpg"
